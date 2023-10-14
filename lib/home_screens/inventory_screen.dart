8a0de5f24682c8e99c60818564dcbc9f5af0e3c5
shru2004 <shrutipatil10951@gmail.com>
2023-10-14 14:30:12 +0530

    Dashboard Firebase Integration

--- lib/home_screens/inventory_screen.dart
+++ lib/home_screens/inventory_screen.dart
@@ -1,8 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medipal/models/MedicationModel.dart';
 
-class InventoryScreen extends StatelessWidget {
-  const InventoryScreen({Key? key}) : super(key: key);
+FirebaseAuth auth = FirebaseAuth.instance;
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+final userId = auth.currentUser?.uid;
+
+class InventoryScreen extends StatefulWidget {
+  const InventoryScreen({super.key});
+
+  @override
+  _InventoryScreenState createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends State<InventoryScreen> {
+  Future<List<QueryDocumentSnapshot>?> fetchData() async {
+    final medicationQuery = firestore
+        .collection('medications')
+        .where('userId', isEqualTo: userId)
+        .get();
+
+    List<QueryDocumentSnapshot> medicationDocumentList = [];
+
+    try {
+      final results = await Future.wait([medicationQuery]);
+      final medicationQuerySnapshot = results[0] as QuerySnapshot;
+
+      if (medicationQuerySnapshot.docs.isNotEmpty) {
+        medicationDocumentList = medicationQuerySnapshot.docs.toList();
+      }
+      return medicationDocumentList;
+    } catch (error) {
+      print('Error retrieving documents: $error');
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,24 +44,51 @@ class InventoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Inventory'),
       ),
-      body: _buildInventoryList(), // Create the inventory list view
+      body: Column(
+        children: [
+          Expanded(child: FutureBuilder(
+            future: fetchData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                final medicationQuery = snapshot.data;
+                return _buildInventoryCard(medicationQuery!);
+              }
+            },
+          ))
+        ],
+      ), // Create the inventory list view
     );
   }
+
 
   Widget _buildInventoryList() {
     // Replace this with your actual inventory data or use a ListView.builder
     return ListView(
       children: [
-        _buildInventoryCard(),
-        _buildInventoryCard(),
-        _buildInventoryCard(),
-        _buildInventoryCard(),
+        // _buildInventoryCard(),
+        // _buildInventoryCard(),
+        // _buildInventoryCard(),
+        // _buildInventoryCard(),
         // Add more inventory cards here
       ],
     );
   }
 
-  Widget _buildInventoryCard() {
+  Widget _buildInventoryCard(
+      List<QueryDocumentSnapshot> medicationQuerySnapshot) {
+    return ListView.builder(
+        itemCount: 4, itemBuilder: (BuildContext context, int index) {
+          final QueryDocumentSnapshot medicationDocumentSnapshot= medicationQuerySnapshot[index];
+          final MedicationModel medicationModel= MedicationModel.fromDocumentSnapshot(medicationDocumentSnapshot);
+          final Map<String, dynamic> medication = medicationModel.toMap();
+          final name= medication['name'];
+          final quantity= medication['inventory']['quantity'];
+          final recorder= medication['inventory']['recorderLevel'];
+    });
     return Card(
       margin: const EdgeInsets.all(8),
       child: Column(
