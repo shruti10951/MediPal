47caec7b456bf991ce4e1eb72ccf46085f93d60c
KJanhavi04 <k.janhavi04@gmail.com>
2023-10-25 13:19:17 +0530

    tab bar added

---
+++ lib/Dependent/tab_change.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:medipal/Dependent/gaurdian_view_screen.dart';
+import 'package:medipal/Individual/inventory_screen.dart';
+
+class TabChange extends StatelessWidget {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [const GaurdianView(), const InventoryScreen()];
+
+  TabChange({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Top Navigation Demo'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.home,
+                  color: Color.fromRGBO(
+                      41, 45, 92, 1.0), // Set the color for the Home icon
+                ),
+                text: 'Home',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Color.fromRGBO(
+                      41, 45, 92, 1.0), // Set the color for the Inventory icon
+                ),
+                text: 'Inventory',
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: _tabs,
+        ),
+      ),
+    );
+  }
+}
