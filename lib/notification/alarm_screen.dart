909777ec980172ab666af1037134d013f596b4cf
shru2004 <shrutipatil10951@gmail.com>
2023-10-26 22:54:48 +0530

    Alarm Scheduling

--- lib/notification/alarm_screen.dart
+++ lib/notification/alarm_screen.dart
@@ -4,14 +4,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:medipal/Individual/bottom_navigation_individual.dart';
 import 'package:medipal/models/AlarmModel.dart';
+import 'package:medipal/models/MedicationModel.dart';
+import 'package:twilio_flutter/twilio_flutter.dart';
 
+import '../credentials/firebase_cred.dart';
+import '../credentials/twilio_cred.dart';
 import '../main.dart';
 
 class AlarmScreen extends StatefulWidget {
   final String alarmId;
 
   AlarmScreen({required this.alarmId});
-
 
   @override
   _AlarmScreenState createState() => _AlarmScreenState();
@@ -21,64 +24,88 @@ class AlarmScreen extends StatefulWidget {
 class _AlarmScreenState extends State<AlarmScreen> {
 
   late AlarmModel alarm;
+  late MedicationModel medication;
+
   Map<String, dynamic> alarmMap={};
+  Map<String, dynamic> medicationMap= {};
+
   final user= FirebaseAuth.instance.currentUser;
-  @override
-  void initState() {
-    super.initState();
-    loadAlarmData();
-  } // Dynamic data
-  String time = '10:00 AM'; // Replace with your time
-  String medicineType = 'Liquid'; // Replace with your medicine type
-  String description =
-      'Your medias you want without affecting the layout.'; // Replace with your description
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Large Alarm Icon
-            AlarmIcon(),
-            const SizedBox(height: 10),
-
-            // Dynamic Time
-            Text(
-              time,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Medicine Type Icon
-            MedicineTypeIcon(medicineType: medicineType),
-            const SizedBox(height: 10),
-
-            // Medicine Description
-            Container(
-              height: 300, // Fixed height for the description container
-              child: MedicineDescription(description: description),
-            ),
-            // Action Buttons
-            ActionButtons(alarmId: widget.alarmId, alarmMap: alarmMap),
-          ],
-        ),
-      ),
+      body: FutureBuilder<void>(
+        future: loadData(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return CircularProgressIndicator();
+          }else if(snapshot.hasError){
+            return Text('Error: ${snapshot.error}');
+          }else{
+            return buildUI();
+          }
+        },
+      )
     );
   }
 
-  void loadAlarmData()async {
-    QuerySnapshot snapshot= await FirebaseFirestore.instance.collection('alarms').where('alarmId', isEqualTo: widget.alarmId).get();
-    for(QueryDocumentSnapshot s in snapshot.docs){
+  Future<void> loadData() async{
+    alarmMap= await loadAlarmData();
+    medicationMap= await loadMedicationData();
+  }
+
+  Future<Map<String, dynamic>> loadMedicationData() async{
+    QuerySnapshot medicationSnapshot= await FirebaseFirestore.instance.collection('medications').where('medicationId', isEqualTo: alarmMap['medicationId']).get();
+    for(QueryDocumentSnapshot s in medicationSnapshot.docs){
+      medication= MedicationModel.fromDocumentSnapshot(s);
+      medicationMap= medication.toMap();
+    }
+    return medicationMap;
+  }
+
+  Future<Map<String, dynamic>> loadAlarmData()async {
+    QuerySnapshot alarmSnapshot= await FirebaseFirestore.instance.collection('alarms').where('alarmId', isEqualTo: widget.alarmId).get();
+    for(QueryDocumentSnapshot s in alarmSnapshot.docs){
       alarm= AlarmModel.fromDocumentSnapshot(s);
       alarmMap= alarm.toMap();
     }
+    return alarmMap;
+  }
+
+  Widget buildUI() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Large Alarm Icon
+          AlarmIcon(),
+          const SizedBox(height: 10),
+          // Dynamic Time
+          Text(
+            alarmMap['time'] ?? 'No time available',
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          // Medicine Type Icon
+          MedicineTypeIcon(medicineType: medicationMap['type'] ?? 'Pills'),
+          const SizedBox(height: 10),
+
+          // Medicine Description
+          Container(
+            height: 300, // Fixed height for the description container
+            child: MedicineDescription(description: medicationMap['description'] ?? 'No description available'),
+          ),
+          // Action Buttons
+          ActionButtons(alarmId: widget.alarmId, alarmMap: alarmMap),
+        ],
+      ),
+    );
   }
 }
 
@@ -212,8 +239,29 @@ class ActionButtons extends StatelessWidget {
               child: const Text('Cancel'),
             ),
             ElevatedButton(
-              onPressed: () {
-                // Handle cancellation here
+              onPressed: () async {
+                  final data = await FirebaseCred().getData();
+                  final user = data[0];
+                  final cred = await TwilioCred().readCred();
+                  final guardian = await FirebaseCred().getGuardianData(user.uid.toString());
+                  TwilioFlutter twilioFlutter;
+
+                  if (guardian != null) {
+                    twilioFlutter = TwilioFlutter(
+                      accountSid: cred[0],
+                      authToken: cred[1],
+                      twilioNumber: cred[2],
+                    );
+
+                    twilioFlutter.sendSMS(
+                      toNumber: '+91' + guardian['phoneNo'],
+                      messageBody: 'Your dependent did not take the medicine! \nReason: $reasonController.text',
+                    );
+                    navigatorKey.currentState?.pushReplacement(MaterialPageRoute(builder: (context)=> BottomNavigationIndividual()));
+                  } else {
+                    // Handle the case where guardian is null (e.g., show an error message).
+                    print('Guardian data is not available.');
+                  }
 
                 Navigator.of(context).pop();
               },
