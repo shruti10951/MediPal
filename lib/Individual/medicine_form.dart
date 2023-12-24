bcdca944c435d6684bfac841f001922fd6fd44d9
KJanhavi04 <k.janhavi04@gmail.com>
2023-12-24 11:51:11 +0530

    namita commit

--- lib/Individual/medicine_form.dart
+++ lib/Individual/medicine_form.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:medipal/Individual/bottom_navigation_individual.dart';
